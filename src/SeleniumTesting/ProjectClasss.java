2c1998ae65dd285720e4ff880c1f94ccf0e39923
shyambalaji70 <shyambalaji70@gmail.com>
2019-08-15 13:09:24 +0530

    Import Completed

---
+++ src/SeleniumTesting/ProjectClasss.java
@@ -0,0 +1,14 @@
+package SeleniumTesting;
+
+public class ProjectClasss {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c;
+		c=a+b;
+		System.out.println(c);
+		
+		
+	}
+
+}
