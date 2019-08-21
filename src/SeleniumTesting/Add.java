8a6244710af401c27535c5d06c157a44250228af
shyambalaji70 <shyambalaji70@gmail.com>
2019-08-21 10:52:24 +0530

    new line added

--- src/SeleniumTesting/Add.java
+++ src/SeleniumTesting/Add.java
@@ -4,10 +4,12 @@ public class Add {
 	public static void main(String[] args) {
 		int a=1;
 		int b=2;
+		int d=3;
 		System.out.println(a);
 		System.out.println(b);
 		int c;
-		c=a+b;
+		
+		c=a+b+d;
 		System.out.println(c);
 	}
 	
