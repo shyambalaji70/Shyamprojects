67c2b8d73341e3331ecf64ac9501c8f6ac7b6db6
shyambalaji70 <shyambalaji70@gmail.com>
2019-08-21 10:50:17 +0530

    New Add

---
+++ src/SeleniumTesting/Add.java
@@ -0,0 +1,20 @@
+package SeleniumTesting;
+
+public class Add {
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		System.out.println(a);
+		System.out.println(b);
+		int c;
+		c=a+b;
+		System.out.println(c);
+	}
+	
+	
+
+	}
+
+		
+
+
